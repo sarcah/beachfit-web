06589b59b062485df1459e49e859a9bda928e864
PunitDh <cas012115@coderacademy.edu.au>
2021-07-20 19:35:28 +1000

    Debug homepage

--- src/front/Home.jsx
+++ src/front/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from "./components/Navbar";
 import MainSVG1 from "./svg/MainSVG1";
 import MainSVG2 from "./svg/MainSVG2";
@@ -7,347 +7,80 @@ import { Link } from 'react-router-dom';
 import background from "./cover.jpg";
 import beachfitLogo from "./beachfit-logo.png";
 import S3FileUpload from "react-s3";
+import axios from "axios";
+import { API_URL } from '../api/auth';
+import heroImage from "./GMQtRz5w.jpeg"
 
 
 const Home = () => {
+
+	const [testimonials, setTestimonials] = useState();
+
+	useEffect(() => {
+		axios.get(`${API_URL}/testimonials/sample/3`)
+			.then(response => setTestimonials(response.data)).catch();
+	}, []);
+
 	return (
-		<div className="bg-gray-600 font-sans leading-normal tracking-normal">
-			{/*Header*/}
-			<Link to="/" className="mt-12 ml-24 absolute"><img src={beachfitLogo} /></Link>
-			<div className="w-full m-0 p-0 bg-cover bg-bottom" style={{
-				backgroundImage: `url(${background})`,
-				backgroundSize: "cover",
-				height: "85vh",
-				maxHeight: 460
-			}}>
-				<div className="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
-					{/*Title*/}
-				</div>
-			</div>
-			{/*Container*/}
-			<div className="container px-4 md:px-0 md:w-4/5 mx-auto -mt-32">
-				<div className="mx-0 sm:mx-6">
-					{/*Nav*/}
-					<nav className="mt-0 w-full">
-						<div className="container mx-auto flex items-center bg-white">
-							<div className="flex w-1/2 pl-4 text-sm">
-								<ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
-									<li className="mr-2">
-										<Link className="inline-block py-2 px-2 text-black no-underline hover:underline" to="/about">About</Link>
-									</li>
-									<li className="mr-2">
-										<a className="inline-block py-2 px-2 text-black no-underline hover:underline" href="#">Contact</a>
-									</li>
-									<li className="mr-2">
-										<a className="inline-block py-2 px-2 text-black no-underline hover:underline" href="#">FAQ</a>
-									</li>
-									<li className="mr-2">
-										<a className="inline-block py-2 px-2 text-black no-underline hover:underline" href="post_vue.html">Timetable</a>
-									</li>
-									<li className="mr-2">
-										<a className="inline-block py-2 px-2 text-black no-underline hover:underline" href="post_vue.html">Pricing</a>
-									</li>
-								</ul>
-							</div>
-							<div className="flex w-1/2 justify-end content-center">
-								<a className="inline-block text-gray-500 no-underline hover:text-white hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 avatar"
-									data-tippy-content="@twitter_handle" href="https://twitter.com/intent/tweet?url=#">
-									<svg className="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-										<path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z" />
-									</svg>
-								</a>
-								<a className="inline-block text-gray-500 no-underline hover:text-white hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 avatar"
-									data-tippy-content="#facebook_id" href="https://www.facebook.com/sharer/sharer.php?u=#">
-									<svg className="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-										<path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z" />
-									</svg>
-								</a>
-							</div>
-						</div>
-					</nav>
-					<div className="bg-gray-200 w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t">
+		<>
+		<Navbar />
+		<div className="container md:w-4/5 mx-auto text-gray-800 leading-normal mb-64">
+				<div className="flex flex-col text-center justify-center h-full bg-white rounded shadow-lg pt-8 mx-0 sm:mx-6">
 						{/*Lead Card*/}
-						<div className="flex h-full bg-white rounded overflow-hidden shadow-lg">
-							<a href="post.html" className="flex flex-wrap no-underline hover:no-underline">
-								<div className="w-full md:w-2/3 rounded-t">
-									<img src="https://source.unsplash.com/collection/494263/800x600" className="h-full w-full shadow" />
-								</div>
-								<div className="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
-									<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-										<p className="w-full text-gray-600 text-xs md:text-sm pt-6 px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											👋 Welcome fellow Tailwind CSS and Ghost fan
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											This starter template is an attempt to replicate the default
-											Ghost theme "Casper" using Tailwind CSS and vanilla
-											Javascript.
-										</p>
+						<div className="flex h-full flex-col text-center justify-center bg-white rounded overflow-hidden shadow-lg">
+							<div className="w-full flex flex-col flex-grow flex-shrink">
+								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+									<div className="w-full font-bold text-3xl text-gray-900 px-6 mb-6">
+										It’s time to put YOU first!
 									</div>
-									<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-										<div className="flex items-center justify-between">
-											<img
-												className="w-8 h-8 rounded-full mr-4 avatar"
-												data-tippy-content="Author Name"
-												src="http://i.pravatar.cc/300"
-												alt="Avatar of Author"
-											/>
-											<p className="text-gray-600 text-xs md:text-sm">
-												1 MIN READ
-											</p>
-										</div>
+									<div className="text-gray-800 text-xl text-base px-6 mb-5">
+										At BeachFit and Wellbeing we are passionate about helping you become fitter, stronger and healthier. Our workouts on the beautiful Coogee Beach, are designed to motivate and inspire you on your fitness journey, wherever you may be.
 									</div>
 								</div>
-							</a>
+							</div>
+							<div className="w-full rounded-t">
+								<img src={heroImage} className="h-full w-full shadow" />
+							</div>
+
+
 						</div>
 						{/*/Lead Card*/}
 						{/*Posts Container*/}
 						Testimonials
 						<div className="flex flex-wrap justify-between pt-12 -mx-6">
 							{/*1/3 col */}
-							<div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/225/800x600"
-											className="h-64 w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED1
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
+							{
+								testimonials && testimonials.map(testimonial => {
+									return (
+										<div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+											<div className="flex-1 bg-white rounded-t rounded-b-none shadow-lg">
+
+												<img
+													src="https://source.unsplash.com/collection/225/800x600"
+													className="h-64 w-full rounded-t pb-6" alt=""
+												/>
+
+												<div className="w-full font-bold text-xl text-gray-900 px-6">
+													{testimonial.name}
+												</div>
+												<p className="text-gray-800 text-sm px-6 mb-5">
+													{testimonial.body}
+												</p>
+
+											</div>
 										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
-							{/*1/3 col */}
-							<div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/3106804/800x600"
-											className="h-64 w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-											ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-											at ip Aliquam at ipsum eu nunc commodo posuere et sit amet
-											ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
-							{/*1/3 col */}
-							<div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/539527/800x600"
-											className="h-64 w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full  font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum eu nunc commodo posuere et sit amet ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
+									)
+								})
+							}
+
+
 							{/*1/2 col */}
-							<div className="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/3657445/800x600"
-											className="h-full w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum eu nunc commodo posuere et sit amet ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
+							
 							{/*1/2 col */}
-							<div className="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 flex-row bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/764827/800x600"
-											className="h-full w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum eu nunc commodo posuere et sit amet ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
+							
 							{/*2/3 col */}
-							<div className="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/325867/800x600"
-											className="h-full w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum eu nunc commodo posuere et sit amet ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
-							{/*1/3 col */}
-							<div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-								<div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-									<a
-										href="#"
-										className="flex flex-wrap no-underline hover:no-underline"
-									>
-										<img
-											src="https://source.unsplash.com/collection/1118905/800x600"
-											className="h-full w-full rounded-t pb-6"
-										/>
-										<p className="w-full text-gray-600 text-xs md:text-sm px-6">
-											GETTING STARTED
-										</p>
-										<div className="w-full font-bold text-xl text-gray-900 px-6">
-											Lorem ipsum dolor sit amet.
-										</div>
-										<p className="text-gray-800 font-serif text-base px-6 mb-5">
-											Lorem ipsum eu nunc commodo posuere et sit amet ligula.
-										</p>
-									</a>
-								</div>
-								<div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-									<div className="flex items-center justify-between">
-										<img
-											className="w-8 h-8 rounded-full mr-4 avatar"
-											data-tippy-content="Author Name"
-											src="http://i.pravatar.cc/300"
-											alt="Avatar of Author"
-										/>
-										<p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-									</div>
-								</div>
-							</div>
+							
+							
 						</div>
 						{/*/ Post Content*/}
 					</div>
@@ -406,8 +139,6 @@ const Home = () => {
 						</div>
 					</div>
 					{/*/Author*/}
-				</div>
-			</div>
 			<footer className="bg-gray-900">
 				<div className="container max-w-6xl mx-auto flex items-center px-2 py-8">
 					<div className="w-full mx-auto flex flex-wrap items-center">
@@ -459,6 +190,7 @@ const Home = () => {
 				</div>
 			</footer>
 		</div>
+		</>
 	);
 }
 
